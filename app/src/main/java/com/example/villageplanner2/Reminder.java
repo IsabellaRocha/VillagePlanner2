323ec4cd2475c6b0425890a798c8416648dfc3ff
Isabella <irocha@usc.edu>
2022-11-03 14:43:26 -0700

    Made it so AM and PM times are displayed correctly

--- app/src/main/java/com/example/villageplanner2/Reminder.java
+++ app/src/main/java/com/example/villageplanner2/Reminder.java
@@ -20,10 +20,23 @@ public class Reminder {
         return destination;
     }
     public String getTimeDisplay() {
+        String AMOrPM = "AM";
         SimpleDateFormat currTimeFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm");
         Date currTime = new Date(time);
         String currTimeString = currTimeFormat.format(currTime);
-        String hourAndMin = currTimeString.substring(11, 16);
+        int hour = Integer.parseInt(currTimeString.substring(11, 13));
+        int min = Integer.parseInt(currTimeString.substring(14, 16));
+        if (hour > 12) {
+            AMOrPM = "PM";
+            hour -= 12;
+        }
+        else if(hour == 0) {
+            hour = 12;
+        }
+        else if(hour == 12) {
+            AMOrPM = "PM";
+        }
+        String hourAndMin = String.format("%02d:%02d %s", hour, min, AMOrPM);
         return hourAndMin;
     }
     public long getTime() {
