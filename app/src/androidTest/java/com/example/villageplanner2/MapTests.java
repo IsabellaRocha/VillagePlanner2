a7901ff2705318ba66bd643d333a4b67697d15af
Isabella <irocha@usc.edu>
2022-11-21 20:52:50 -0800

    gradle stuff for map test

--- app/src/androidTest/java/com/example/villageplanner2/MapTests.java
+++ app/src/androidTest/java/com/example/villageplanner2/MapTests.java
@@ -3,6 +3,8 @@ package com.example.villageplanner2;
 import static androidx.test.espresso.Espresso.onData;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.platform.app.InstrumentationRegistry.getInstrumentation;
 
@@ -18,22 +20,29 @@ import androidx.test.uiautomator.UiObject;
 import androidx.test.uiautomator.UiObjectNotFoundException;
 import androidx.test.uiautomator.UiSelector;
 
-import com.example.villageplanner2.HomeLogic.HomepageActivity;
-import com.example.villageplanner2.R;
 
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+
+@RunWith(AndroidJUnit4.class)
+@LargeTest
 public class MapTests {
+    private String email = "irocha@usc.edu";
+    private String password = "123456";
     @Rule
-    public ActivityScenarioRule<MapActivity> activityRule = new ActivityScenario<MapActivity>(MapActivity.class);
+    public ActivityScenarioRule<LandingActivity> activityRule = new ActivityScenarioRule<>(LandingActivity.class);
 
     @Test
-    public void displayRouteTest() throws UIObjectNotFoundException {
-        onView(withId(R.id.store)).perform(click());
+    public void displayRouteTest() throws InterruptedException {
+        onView(withId(R.id.login)).perform(click());
+        onView(withId(R.id.email)).perform(typeText(email), closeSoftKeyboard());
+        onView(withId(R.id.password)).perform(typeText(password), closeSoftKeyboard());
+        onView(withId(R.id.login)).perform(click());
+        Thread.sleep(5000);
+        //onView(withId(R.id.store)).perform(click());
         onData(allOf(is(instanceOf(String.class)), is("CAVA"))).perform(click());
-        onView(withId(R.id.display_route)).perform(click());
         UiDevice device = UiDevice.getInstance(getInstrumentation());
         UiObject storeMarkerOne = device.findObject(new UiSelector().descriptionContains("CAVA"));
         UiObject myMarkerOne = device.findObject(new UiSelector().descriptionContains("My Location"));
